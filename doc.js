f4df089fb0d6a417e10274b440012854a43f69f3
DidierShemaGatete <didiershemagate01@gmail.com>
2023-06-10 11:38:56 +0300

    objects

--- doc.js
+++ doc.js
@@ -1,2 +1,34 @@
 /* constuctor functions and factorial functions are used to creat objects
  */
+
+var person = {
+    name: 'John',
+    age: 30,
+    gender: 'male',
+    sayHello: function() {
+      console.log('Hello!');
+    }
+  };
+  
+  var propertyName = 'name';
+  console.log(person[propertyName]); // Accessing property using bracket notation
+  
+  var methodName = 'sayHello';
+  person[methodName](); // Invoking method using bracket notation
+  
+
+  var obj1 = { a: 1 };
+var obj2 = { b: 2 };
+var obj3 = { c: 3 };
+
+var mergedObject = {};
+for (var key in obj1) {
+  mergedObject[key] = obj1[key];
+}
+for (var key in obj2) {
+  mergedObject[key] = obj2[key];
+}
+for (var key in obj3) {
+  mergedObject[key] = obj3[key];
+}
+console.log(mergedObject); // Output: { a: 1, b: 2, c: 3 }
